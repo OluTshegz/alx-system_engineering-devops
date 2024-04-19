16dad2ee6b7f6ec1bb7005f7c02271b5f301bdae
Olusegun <ayosegun2001@gmail.com>
2024-04-19 18:49:02 +0100

    0x0A. Configuration management: DevOps, SysAdmin, Scripting, CI/CD

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Using Puppet, install a specific version of flask (2.1.0) from pip3
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
