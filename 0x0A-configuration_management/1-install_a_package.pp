58b43a6cf00912cfa9611ae53ad049d40928cecb
Olusegun <ayosegun2001@gmail.com>
2024-04-19 13:55:51 +0100

    0x0A. Configuration management: DevOps, SysAdmin, Scripting, CI/CD

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Using Puppet, install a specific version of flask (2.1.0) from pip3
-# Ensure pip3 is installed (adjust if needed)
 package {   'python3-pip':
-  ensure   => installed,
+  ensure   => installed,  # Ensure pip3 is installed (adjust if needed)
 }
 
 # Install Flask using pip3
