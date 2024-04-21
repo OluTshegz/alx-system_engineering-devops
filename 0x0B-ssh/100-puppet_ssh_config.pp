a851741be8252779c130633b3c18919885df66a9
Olusegun <ayosegun2001@gmail.com>
2024-04-21 18:54:12 +0100

    0x0B. SSH: DevOps, SSH, Network, SysAdmin, Security

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,17 @@
 # using Puppet to make changes to our configuration file by setting up your client
 # SSH configuration file so that you can connect to a server without typing a password
 
+include stdlib
+
 # Define a resource to manage the "IdentityFile" line
-file_line { 'Declare identity file':
-  ensure             => present
-  path               => '/etc/ssh/ssh_config'
-  line               => '    IdentityFile ~/.ssh/school'
-  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
-  replace            => true,
-  append_on_no_match => true
-}
+    file_line { 'Declare identity file':
+      ensure             => present,
+      path               => '/etc/ssh/ssh_config',
+      line               => '    IdentityFile ~/.ssh/school',
+      match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+      replace            => true,
+      append_on_no_match => true,
+    }
 
 # Regex match explanation
 #
@@ -22,14 +24,14 @@ file_line { 'Declare identity file':
 # $      end of the line
 
 # Define a resource to manage the "PasswordAuthentication" line
-file_line { 'Turn off passwd auth':
-  ensure             => present
-  path               => '/etc/ssh/ssh_config'
-  line               => '    PasswordAuthentication no'
-  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
-  replace            => true,
-  append_on_no_match => true
-}
+    file_line { 'Turn off passwd auth':
+      ensure             => present,
+      path               => '/etc/ssh/ssh_config',
+      line               => '    PasswordAuthentication no',
+      match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+      replace            => true,
+      append_on_no_match => true,
+    }
 
 # Regex match explanation
 #
