c70ecd55746a1426bf40e5fa82eeaf75e160fec9
Olusegun <ayosegun2001@gmail.com>
2024-04-30 02:30:21 +0100

    0x0F. Load balancer: DevOps, SysAdmin

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,17 @@
 # Using Puppet to automate the task of creating a custom HTTP header response
 # configures a brand new Ubuntu machine to the requirements asked in this task
 
-exec { 'update':
+exec {'update':
   command => '/usr/bin/apt-get update',
-  path    => '/usr/bin/',
 }
-
-package { 'nginx':
-  ensure  => 'present',
-  require => Exec['update'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-file_line { 'http_header':
-  path    => '/etc/nginx/nginx.conf',
-  match   => 'http {',
-  line    => '        add_header X-Served-By $hostname};',
-  require => Package['nginx'],
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-exec { 'restart_nginx':
+-> exec {'run':
   command => '/usr/sbin/service nginx restart',
-  require => File_line['http_header'],
 }
