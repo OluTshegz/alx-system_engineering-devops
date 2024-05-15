f0757a7b1998970bbfd4c00027f2a6448d013a97
Olusegun <ayosegun2001@gmail.com>
2024-05-15 02:02:10 +0100

    0x14. MySQL: DevOps, SysAdmin, MySQL

---
+++ 0x14-mysql/setup_primary_mysql_server.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS 'tyrell_corp';
+USE tyrell_corp;
+
+CREATE TABLE IF NOT EXISTS nexus6 (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(32) NOT NULL);
+INSERT INTO nexus6 (id, name) VALUES (1, Leon);
+
+CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+GRANT SELECT ON tyrell_corp.nexus6 TO 'holberton_user'@'localhost';
+GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
+
+CREATE USER 'replica_user'@'%' IDENTIFIED WITH mysql_native_password BY 'replica_password';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
