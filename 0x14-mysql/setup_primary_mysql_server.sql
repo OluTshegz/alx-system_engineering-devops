57112e79726e44a580c590125df714c89b07e6c5
Olusegun <ayosegun2001@gmail.com>
2024-05-19 19:54:14 +0100

    0x14. MySQL: DevOps, SysAdmin, MySQL

--- 0x14-mysql/setup_primary_mysql_server.sql
+++ 0x14-mysql/setup_primary_mysql_server.sql
@@ -11,3 +11,15 @@ GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
 
 CREATE USER 'replica_user'@'%' IDENTIFIED WITH mysql_native_password BY 'replica_password';
 GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+
+FLUSH PRIVILEGES;
+
+SHOW MASTER STATUS;
+
+CHANGE MASTER TO
+->    MASTER_HOST='master_server_ip',
+->    MASTER_USER='replica_user',
+->    MASTER_PASSWORD='replica_password';
+
+START SLAVE;
+SHOW SLAVE STATUS\G
